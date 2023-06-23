dd10179fad89dc9d444880de67063fe585025650
geoffrey kinuthia <geoffreykinuthia10@gmail.com>
2023-06-23 05:27:38 +0300

    First commit

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify
+from flask import Flask, jsonify
 from flask_migrate import Migrate
 
 from models import db, Bakery, BakedGood
@@ -19,20 +19,72 @@ def index():
     return '<h1>Bakery GET API</h1>'
 
 @app.route('/bakeries')
-def bakeries():
-    return ''
+def get_all_bakeries():
+    all_bakeries = Bakery.query.all()
+    bakeries_data = []
+    for bakery in all_bakeries:
+        bakery_data = {
+            'id': bakery.id,
+            'name': bakery.name,
+            'created_at': bakery.created_at,
+            'updated_at': bakery.updated_at
+        }
+        bakeries_data.append(bakery_data)
+    return jsonify(bakeries_data)
 
 @app.route('/bakeries/<int:id>')
-def bakery_by_id(id):
-    return ''
+def get_bakery_by_id(id):
+    bakery = Bakery.query.filter_by(id=id).first()
+    if bakery:
+        bakery_data = {
+            'id': bakery.id,
+            'name': bakery.name,
+            'created_at': bakery.created_at,
+            'updated_at': bakery.updated_at,
+            'baked_goods': []
+        }
+        for baked_good in bakery.baked_goods:
+            baked_good_data = {
+                'id': baked_good.id,
+                'name': baked_good.name,
+                'price': baked_good.price,
+                'created_at': baked_good.created_at,
+                'updated_at': baked_good.updated_at
+            }
+            bakery_data['baked_goods'].append(baked_good_data)
+        return jsonify(bakery_data), 200
+    else:
+        return jsonify({'message': 'Bakery not found'}), 404
 
 @app.route('/baked_goods/by_price')
-def baked_goods_by_price():
-    return ''
+def get_baked_goods_by_price():
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_data = []
+    for baked_good in baked_goods:
+        baked_good_data = {
+            'id': baked_good.id,
+            'name': baked_good.name,
+            'price': baked_good.price,
+            'created_at': baked_good.created_at,
+            'updated_at': baked_good.updated_at
+        }
+        baked_goods_data.append(baked_good_data)
+    return jsonify(baked_goods_data)
 
 @app.route('/baked_goods/most_expensive')
-def most_expensive_baked_good():
-    return ''
-
+def get_most_expensive_baked_good():
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if baked_good:
+        baked_good_data = {
+            'id': baked_good.id,
+            'name': baked_good.name,
+            'price': baked_good.price,
+            'created_at': baked_good.created_at,
+            'updated_at': baked_good.updated_at
+        }
+        return jsonify(baked_good_data), 200
+    else:
+        return jsonify({'message': 'No baked goods found'}), 404
+    
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -10,11 +10,34 @@ db = SQLAlchemy(metadata=metadata)
 
 class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
+    
+    serialize_rules = ('-baked_goods',)
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    baked_goods = db.relationship('BakedGood', backref='bakery')
+    
+    
+    def __repr__(self):
+        return f"<Bakery ({self.id})>"
+    
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
-
-    id = db.Column(db.Integer, primary_key=True)
     
+    serialize_rules = ('-bakery',)
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
+    
+    
+    def __repr__(self):
+        return f"<BakedGood {self.id}>"
